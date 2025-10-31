cf051f7c3cad43b36fce3a2b799634cb00ace83e
Jorb1776 <57497662+Jorb1776@users.noreply.github.com>
2025-10-31 08:43:41 -0500

    fix: resolve App.js merge conflict

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
@@ -179,30 +178,3 @@ const navLink = {
 };
 
 export default App;
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
->>>>>>> e7f68cda913aaefa28a5a9aa2e491313bff972f0
